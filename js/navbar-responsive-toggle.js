62fa4989a6255718408cd7c27c45e6b10584a14c
Jakub Lewandowski <maszklepe@outlook.com>
2024-12-03 13:51:07 +0100

    code cleaning

--- js/navbar-responsive-toggle.js
+++ js/navbar-responsive-toggle.js
@@ -1,12 +1,26 @@
 const navbarToggle = document.querySelector("#navbar-toggle");
 const navbarMenu = document.querySelector("#navbar-menu");
 const navbarLinksContainer = navbarMenu.querySelector(".navbar-links");
+
 let isNavbarExpanded = navbarToggle.getAttribute("aria-expanded") === "true";
+
+// Funkcja do przełączania widoczności nawigacji
 const toggleNavbarVisibility = () => {
     isNavbarExpanded = !isNavbarExpanded;
     navbarToggle.setAttribute("aria-expanded", isNavbarExpanded);
+    
+    if (isNavbarExpanded) {
+        navbarMenu.classList.add("is-active"); 
+    } else {
+        navbarMenu.classList.remove("is-active");
+    }
 };
 
+// Kliknięcie na przycisk toggle przełącza widoczność
 navbarToggle.addEventListener("click", toggleNavbarVisibility);
+
+// Kliknięcie wewnątrz kontenera linków nie powoduje zamknięcia menu
 navbarLinksContainer.addEventListener("click", (e) => e.stopPropagation());
+
+// Kliknięcie na menu zamknie je
 navbarMenu.addEventListener("click", toggleNavbarVisibility);
